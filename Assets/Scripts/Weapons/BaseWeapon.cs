ccdba5ef7778af921269dd37dd19a728a7d76294
jarett <jarett@wustl.edu>
2016-03-25 16:31:57 -0500

    added character classes and weapons

---
+++ Assets/Scripts/Weapons/BaseWeapon.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BaseWeapon : MonoBehaviour {
+
+	protected float damageAmount;
+
+	protected bool needsReload;
+	protected int reloadAmount;
+
+	protected float hitboxRadius;
+
+	protected float range;
+	protected float cooldown;
+
+	public GameObject bulletHole;
+	public GameObject bloodHole;
+	public GameObject debrisPrefab;
+
+	public AudioClip pistol_sound;
+
+	//store info of the obeject being hit
+	protected RaycastHit hitInfo;
+
+	//store the type of bulletholes 
+	protected GameObject tempBulletHole;
+
+	//(temporary solution)the remaining time before able to make the next shot
+	protected float cooldownRemaining = 0;
+
+	[SerializeField]    //use this to set the gun camera
+	protected GameObject weaponCamera;
+
+	[SerializeField]   //use this camera to guide the shooting direction
+	protected Camera shootingDirection;
+
+	[SerializeField]
+	protected LayerMask mask;
+
+	public BaseWeapon(float _damageAmount, bool _needsReload, int _reloadAmount, float _hitboxRadius, float _range, float _cooldown, string weaponCameraLayer) {
+		damageAmount = _damageAmount;
+		needsReload = _needsReload;
+		reloadAmount = _reloadAmount;
+		hitboxRadius = _hitboxRadius;
+		range = _range;
+		cooldown = _cooldown;
+
+		weaponCamera.layer = LayerMask.NameToLayer(weaponCameraLayer);
+		foreach (Transform child in weaponCamera.transform) {
+			child.gameObject.layer = LayerMask.NameToLayer(weaponCameraLayer);
+		}
+	}
+}
---
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pistol : BaseWeapon {
+
+	public Pistol() : base(50.0f, false, 0, 1.0f, 100.0f, 0.3f, "Gun") {
+		
+	}
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
