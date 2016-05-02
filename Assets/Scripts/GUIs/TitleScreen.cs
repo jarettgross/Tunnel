b670704b26a3062a81a9c8d3e008a2743aea0089
jarett <jarett@wustl.edu>
2016-05-02 12:34:15 -0500

    more instructions

--- Assets/Scripts/GUIs/TitleScreen.cs
+++ Assets/Scripts/GUIs/TitleScreen.cs
@@ -5,24 +5,37 @@ using UnityEngine.SceneManagement;
 public class TitleScreen : MonoBehaviour {
 
 	public bool showInstructions = false;
+	public bool showHowTo = false;
+
 	public Texture blackBackground;
 	public Texture instructions;
+	public Texture howToPlay;
 
 	void OnGUI() {
 		if (showInstructions) {
 			Rect rect = new Rect(0, 0, Screen.width, Screen.height);
 			GUI.DrawTexture (rect, blackBackground, ScaleMode.StretchToFill);
 			GUI.DrawTexture (rect, instructions, ScaleMode.ScaleToFit);
-			if (Input.GetMouseButtonDown (0)) {
+			if (Input.GetMouseButtonDown (0) && !showHowTo) {
 				if (rect.Contains (Event.current.mousePosition)) {
 					SceneManager.LoadScene ("Loader Scene");
+				}
+			}
+			showHowTo = false;
+		} else if (showHowTo) {
+			Rect rect = new Rect(0, 0, Screen.width, Screen.height);
+			GUI.DrawTexture (rect, blackBackground, ScaleMode.StretchToFill);
+			GUI.DrawTexture (rect, howToPlay, ScaleMode.ScaleToFit);
+			if (Input.GetMouseButtonDown (0)) {
+				if (rect.Contains (Event.current.mousePosition)) {
+					showInstructions = true;
 				}
 			}
 		}
 	}
 
 	public void onInstructionClick() {
-		showInstructions = true;
+		showHowTo = true;
 	}
 
 	public void onPlayClick() {
